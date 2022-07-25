817e8f90c05221047e918b6250b92b4aa87badbd
james green <jamesgreen.311@gmail.com>
2022-07-25 17:30:47 -0400

    Fetch Exhibit Name and Display

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
-var route = getRoute()
+const route = getRoute()
+const EP_CFEAPI = "https://script.google.com/macros/s/AKfycbx0NnV9wQTAkYzvIgqEms8C_HZFTRTjSs5radWTwtMj0xs6hIBm8my6QtAThuytVWY/exec" +
+    "?q="
 var test = true
-
-function retrieveExhibitName() {
-    const id = route.id
-
-    //fetch()
-}
 
 function displayExhibitName(name) {
     const ele = document.getElementById("exhibit-title")
@@ -23,6 +19,34 @@ function mode() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", getRoute)
-document.addEventListener("DOMContentLoaded", retrieveExhibitName)
+function getExhibit() {
+    const exhibit = route.path
+    // path = oldest, get first row from Open Calls
+    // path = newest, get last row from Open Calls
+    // path = all others, assume exhibit id, filter Open Calls for exhibit id
+    const openCalls = getOpenCalls()
+    const exhibitName = ""
+}
+
+function loadPageElements() {
+    fetchOpenCalls()
+
+    console.log(route)
+}
+
+
+function fetchOpenCalls() {
+    const url = EP_CFEAPI + "opencalls"
+    fetch(url) 
+    .then(resp => resp.json())
+    .then(resp => {
+        let name = resp[2]
+        displayExhibitName(name)
+    })
+    .catch()
+}
+
+
+//document.addEventListener("DOMContentLoaded", getRoute)
+document.addEventListener("DOMContentLoaded", loadPageElements)
 document.addEventListener("DOMContentLoaded", mode)
--- src/js/router.js
+++ src/js/router.js
@@ -1,25 +1,13 @@
 function getRoute() {
-    const urlString = window.location.href
-    const paramString = urlString.split('?')[1]
-    const queryString = new URLSearchParams(paramString)
+    const urlString = window.location.search
     let route = {}
-/* 
-    console.log(paramString)
-    console.log(queryString) */
 
-    for(let pair of queryString.entries()) {
-/*         console.log("Key is:" + pair[0]);
-        console.log("Value is:" + pair[1]); */
+    const params = new URLSearchParams(urlString)
 
-        if (!pair[1]) {
-            console.log("Route is %s", pair[0])
-            route.path = pair[0]
-        } else {
-            console.log(pair[0], "=", pair[1])
-            route[pair[0]] = pair[1]
-        }
-    }
-/*     console.log(route.path)
-    console.log(route.id) */
+    let id = params.get('id')
+    let mode = params.get('mode')
+    route["path"] = (id?id:'oldest') // default to oldest
+
+    console.log(id, mode, route.path)
     return route
 }
